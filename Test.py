7d8ded8034a19a032b16d8972e41244ebd78ed93
Polkovnikov Tihon <polkovnikov.tikxhom@gmail.com>
2024-10-30 14:43:20 +0500

    Welcome Alexey

--- Test.py
+++ Test.py
@@ -1,10 +1,1 @@
-print("Hello World!")
-<<<<<<< HEAD
-<<<<<<< HEAD
-print("Yeah!") # @BaldinAlexey
-=======
-print("Yeah!")
->>>>>>> 288591b81240af9b7347f05f25a63e35b8b862a9
-=======
-print("Yeah!")
->>>>>>> 288591b81240af9b7347f05f25a63e35b8b862a9
+print("Лёха, у тебя работает?")
